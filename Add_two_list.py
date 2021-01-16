e8dd54f3fd03d51f61a1f01dae610bb127f119cf
gamersdestiny <gamersdestiny@users.noreply.github.com>
2021-01-16 13:36:31 +0000

    Create Add_two_list.py

---
+++ Add_two_list.py
@@ -0,0 +1,12 @@
+list1 = [1,2,3,4,5,6]
+list2 = [1,2,3,4,5,6] 
+list3 = []
+def addTwoList():
+    print('Adding elemts inside list')
+    for i in range(len(list1)):   
+        list3.append(list1[i]+list2[i])
+    print(list3)
+    print('Another(zip) method to add list')
+    for j in zip(list1, list2):
+        print(sum(j), end=" ")
+addTwoList()
