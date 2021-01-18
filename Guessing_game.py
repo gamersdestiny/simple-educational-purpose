66ba972812eaf55d5a9fe352b9c370642066d019
Guna <71771581+gamersdestiny@users.noreply.github.com>
2021-01-18 15:37:17 +0000

    Update Guessing_game.py
    Single or multiplayer choosable version will be coming soon

--- Guessing_game.py
+++ Guessing_game.py
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 def guessing_game():
     Toguess = input("User 1 input the word to be guessed: ")
     Triedword = ""
@@ -20,27 +19,4 @@ def guessing_game():
             print('That is the correct answer :D')
             result = True
             break
-=======
-def guessing_game():
-    Toguess = input("User 1 input the word to be guessed: ")
-    Triedword = ""
-    result = False
-    Timesgussed = 0
-    print('User 2 find the word... :P You have 3 chances')
-
-    while result == False:
-        print(Toguess[0], '_ _ _', Toguess[4], '_ _', Toguess[-1:])
-        Triedword = input('Guess the right word: ')
-        Timesgussed += 1
-        print('Attempt', Timesgussed)
-        if Timesgussed == 3:
-            print('You are out of chances')
-            break
-        if Toguess != Triedword:
-            print('Wrong answer :( Try again')
-        elif Toguess == Triedword:
-            print('That is the correct answer :D')
-            result = True
-            break
->>>>>>> Stashed changes
 guessing_game()
