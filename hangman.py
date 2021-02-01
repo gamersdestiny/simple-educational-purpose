237bd08d84017406f9383dcaed0e162f7127e54b
Guna <71771581+gamersdestiny@users.noreply.github.com>
2021-02-01 17:53:02 +0530

    Add files via upload
    very simple Hangman game, I have included words file with it. If you ever feel dump just remember it took more than 7 for me to complete this basic game

---
+++ hangman.py
@@ -0,0 +1,42 @@
+import random
+import string
+from words import words
+questionWord = random.choice(words)
+dashes = (' _ '*len(questionWord))
+questionWordset = set(questionWord)
+print(dashes)
+def playagain():
+    Pagain = input('Do you wanna play again Yes or No: ').lower()
+    if 'y' or 'yes' in Pagain:
+        find()
+def find():
+    limit = 6
+    chances = 0
+    lenth = 6
+    dashesAsset = set()
+    userAnswer = set()
+    while chances<limit:
+        userInput = input('\nEnter a character: ').lower()
+        if userInput in questionWord:
+            if userInput in userAnswer:
+                print('You have already entered the word')
+            if userInput in questionWord:
+                if userInput in dashesAsset:
+                    continue
+                userAnswer.add(userInput)
+            for letter in questionWord:
+                if letter in userAnswer:
+                    print(letter, end=' ')
+                else:
+                    print(' _ ',end='')
+            if questionWordset == userAnswer:
+                print('\nYou got the right answer')
+                break
+        elif userInput not in questionWord:
+            chances += 1
+            lenth -=1
+            print(f'Wrong character Try again chances left {lenth}')
+    else:
+        print(f'You are out of chances. The answer is {questionWord}')
+
+find()
